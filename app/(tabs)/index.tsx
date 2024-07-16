cef6f88c8fec980231c2c2cfc4ce3606dbd319eb
dr1co <dricomatoba@hotmail.com>
2024-07-16 10:27:48 -0300

    [chore] add api address on .env

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
 import { Image, StyleSheet, Platform } from 'react-native';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import address from '@/assets/api/api';
+
 export default function HomeScreen() {
+  const [data, setData] = useState<string>("");
+
+  useEffect(() => {
+    const request = axios.get(`${address}/example`)
+    request.then((res) => {
+      setData(res.data);
+    })
+    request.catch((err) => {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -16,7 +32,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">{data}</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
---
+++ assets/api/api.ts
@@ -0,0 +1,3 @@
+const address: string = process.env.EXPO_PUBLIC_API_ADDRESS?? "";
+
+export default address;
