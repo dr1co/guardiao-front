4d71d149deee763102dec9e7f348ab45d9e87b84
hlenarocha <helenavitoria1200@gmail.com>
2024-07-17 12:49:41 -0300

    Project reset

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
-import { Text, View } from "react-native";
+//import { Text, View } from "react-native"; // componentes básicos do react native
+import { Title } from "@/components/Title";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+  <Title/>
   );
 }
---
+++ components/Title.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export function Title() {
+  return (
+  <View
+  style={{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }}
+>
+  <Text>Guardião</Text>
+</View>
+  );
+}
