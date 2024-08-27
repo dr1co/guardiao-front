2bb78eea6e9efa6db79c5d1584565d6f29d83d57
hlenarocha <helenavitoria1200@gmail.com>
2024-08-27 10:45:04 -0300

    Axios tested

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+// http://localhost:4000
+const url = process.env.EXPO_PUBLIC_API_ADDRESS;
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: url,
   headers: {
     "Content-Type": 'application/json'
   },
+  
 });
-
 
 export default axiosInstance;
 
