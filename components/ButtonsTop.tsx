71486489fe7da8e630ca49532b8289190b0cd36b
hlenarocha <helenavitoria1200@gmail.com>
2024-08-28 21:13:37 -0300

    Add modal

--- components/ButtonsTop.tsx
+++ components/ButtonsTop.tsx
@@ -1,17 +1,108 @@
-import { View, TouchableOpacity, Image } from 'react-native';
+import { useState } from "react";
+import { View, TouchableOpacity, Image, Modal, Text } from "react-native";
 
 export default function ButtonsTop() {
-  return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: 130}}>
-      <TouchableOpacity style={{width: 45, height: 45, backgroundColor: 'white', borderRadius: 50, alignItems: 'center', justifyContent: 'center'}}>
-        <Image source={require('@/assets/images/bell.png')}/>
-      </TouchableOpacity>
-      <TouchableOpacity style={{width: 45, height: 45, borderRadius: 50, alignItems: 'center', justifyContent: 'center', marginTop: 8}}>
-        <Image source={require('@/assets/images/user_photo.png')}/>
-      </TouchableOpacity>
-      <View>
+  const [showModalUser, setModalUser] = useState(false);
+  const [showModalNotifications, setModalNotifications] = useState(false);
 
-      </View>
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: 130,
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          width: 45,
+          height: 45,
+          backgroundColor: "white",
+          borderRadius: 50,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={() => setModalNotifications(!showModalNotifications)}
+      >
+        <Image source={require("@/assets/images/bell.png")} />
+        <Modal
+          visible={showModalNotifications}
+          animationType="fade"
+          transparent={true}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              borderRadius: 20,
+              padding: 20,
+              width: 300,
+              marginLeft: 50,
+              marginTop: 75,
+              alignItems: "center",
+            }}
+          >
+            <View style={{ padding: 20, paddingLeft: 60, paddingRight: 60, borderRadius: 16, borderWidth: 3, borderColor: '#7b6dd1'}}>
+              <Text
+                style={{ fontSize: 18, color: "black", textAlign: "center" }}
+              >
+                Notificação
+              </Text>
+            </View>
+          </View>
+        </Modal>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          width: 45,
+          height: 45,
+          borderRadius: 50,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 8,
+        }}
+        onPress={() => setModalUser(!showModalUser)}
+      >
+        <Image source={require("@/assets/images/user_photo.png")} />
+        <Modal visible={showModalUser} animationType="fade" transparent={true}>
+          <View
+            style={{
+              backgroundColor: "white",
+              borderRadius: 20,
+              padding: 20,
+              width: 150,
+              marginLeft: 300,
+              marginTop: 75,
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                marginTop: 20,
+                backgroundColor: "red",
+                padding: 10,
+                borderRadius: 10,
+                marginBottom: 10,
+                paddingLeft: 20,
+                paddingRight: 20,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: "white",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}
+              >
+                Sair
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </TouchableOpacity>
+      <View></View>
     </View>
   );
 }
