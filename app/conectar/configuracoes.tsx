6547a8769a2b013db2d6b0dbbe5ab84dec8db851
hlenarocha <helenavitoria1200@gmail.com>
2024-08-30 11:24:58 -0300

    [feat.] Add functional buttons + Switch

--- app/conectar/configuracoes.tsx
+++ app/conectar/configuracoes.tsx
@@ -1,16 +1,24 @@
-import { View, useWindowDimensions, Image, Text, ScrollView } from "react-native";
+import {
+  View,
+  useWindowDimensions,
+  Image,
+  Text,
+  ScrollView,
+} from "react-native";
 import ButtonsTop from "@/components/ButtonsTop";
-import ButtonGeral from "@/components/ButtonGeral";
 import MenuBar from "@/components/MenuBar";
+import ContainerPerfil from "@/components/ContainerPerfil";
+import ButtonConfig from "@/components/ButtonConfig";
+import ButtonConfigSwitch from "@/components/ButtonConfigSwitch";
 
-export default function TelaInicio() {
+export default function TelaConfiguracoes() {
   const { width, height } = useWindowDimensions();
 
   return (
     <View
       style={{
         width,
-        height,
+        height: height,
         flex: 1,
         alignItems: "center",
         backgroundColor: "#7b6dd1",
@@ -36,28 +44,58 @@ export default function TelaInicio() {
           alignItems: "center",
         }}
       >
-         
+        <View
+          style={{
+            backgroundColor: "#e2e1ff",
+            width,
+            height,
+            borderRadius: 50,
+            alignItems: "center",
+          }}
+        >
           
 
-        <ScrollView>
-        
-        </ScrollView>
+        <View style={{height: 730, marginTop: 20}}>
+            <ScrollView>
+            <View style={{ width: 309 }}>
+            <Text
+              style={{
+                fontSize: 24,
+                lineHeight: 48,
+                fontFamily: "IBM Plex Sans",
+                color: "#7b6dd1",
+                fontWeight: "bold",
+                marginTop: 30,
+              }}
+            >
+              Configurações
+            </Text>
+              </View>
+              <ContainerPerfil />
+              <Text style={{ color: '#f48080', fontSize: 15, fontWeight: 'bold', marginBottom: 10}}>Conta</Text>
+              <ButtonConfig text="Informações pessoais" imagePath={require('@/assets/images/user_icon.png')}/>
+              <ButtonConfig text="Crianças conectadas" imagePath={require("@/assets/images/child.png")}/>
+              <ButtonConfig text="Acessibilidade" imagePath={require('@/assets/images/acessibility.png')} />
 
-        <View>
-          <View style={{ position: "absolute", zIndex: 2, width: '100%', alignItems: 'center', marginTop: 15 }}>
-            <Image source={require("@/assets/images/update.png")} />
+              <Text style={{ color: '#f48080', fontSize: 15, fontWeight: 'bold', marginBottom: 10, marginTop: 20}}>Privacidade & Segurança</Text>
+              <ButtonConfig text="Gerenciar informações" imagePath={require('@/assets/images/config.png')}/>
+
+              <Text style={{ color: '#f48080', fontSize: 15, fontWeight: 'bold', marginBottom: 10, marginTop: 20}}>Permissões</Text>
+              <ButtonConfigSwitch text="Notificações" imagePath={require('@/assets/images/notif.png')}/>
+              <ButtonConfigSwitch text="Localização" imagePath={require('@/assets/images/local.png')}/>
+
+
+              <Text style={{ color: '#f48080', fontSize: 15, fontWeight: 'bold', marginBottom: 10, marginTop: 20}}>Suporte</Text>
+              <ButtonConfig text="Ajuda" imagePath={require('@/assets/images/help.png')} />
+              <ButtonConfig text="Sobre o aplicativo" imagePath={require('@/assets/images/info.png')}/>
+              <View style={{height: 100}}></View>
+            </ScrollView>
           </View>
-          <ButtonGeral backgroundColor="#7B6DD1" text="" />
         </View>
       </View>
 
-
-      <MenuBar/>
-      <View>
-        
-      </View>
+      <MenuBar />
+      <View></View>
     </View>
-
-    
   );
 }
